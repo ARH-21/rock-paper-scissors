bddbc3c2095579da5cfa7d5d1a70f4a87d5c3b0f
ARH-21 <armheyd21@gmail.com>
2025-06-21 14:48:50 -0700

    feat: getComputerChoice() choice logic

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log("Hello World")
+console.log("Testing...")
+
+function getComputerChoice()
+{
+    // Return one following string values (rock, paper, scissors)
+    // No arrays, what else could you use --> Conditional Statements, Switch
+
+
+    // We need it to randomly choose numbers 1, 2, 3
+    // The floor will prevent 1.2 or 2.5
+    // The + 1 will make sure it starts 1, 2, 3 not 0-3
+    let computerChoice = Math.floor(Math.random() * 3 + 1);
+
+    if (computerChoice == 1)
+    {
+        return "rock"; 
+    }
+    else if (computerChoice == 2)
+    {
+        return "paper";
+    }
+    else
+    {
+        return "scissors";
+    }
+
+}
