d55eff08596ea146bceca6fdf6c0397ea2904918
ARH-21 <armheyd21@gmail.com>
2025-06-22 16:30:59 -0700

    feat: playGame and playRound function.

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 console.log("Testing...")
 
+
+ // Humans Choice
+function getHumanChoice()
+{
+     // prompt for input
+    // Future fix: Invalid input 
+    const humanChoice = prompt("Enter a choice (rock, paper, scissors:").toLowerCase();
+
+    if (humanChoice == "rock")
+    {
+        return "rock";
+    }
+    else if (humanChoice == "paper")
+    {
+        return "paper";
+    }
+    else
+    {
+        return "scissors";
+    }
+
+}
+
+// Call the function 
+// (testing prompt)
+console.log(getHumanChoice());
+
 function getComputerChoice()
 {
     // Return one following string values (rock, paper, scissors)
@@ -25,28 +52,59 @@ function getComputerChoice()
     }
 }
 
- // Humans Choice
-function getHumanChoice()
-{
-     // prompt for input
-    // Future fix: Invalid input 
-    const humanChoice = prompt("Enter a choice (rock, paper, scissors:");
+// Keep track of score (global scope)
+let humanScore = 0;
+let computerScore = 0;
 
-    if (humanChoice == "rock")
+function playRound(humanChoice, computerChoice)
+{
+    // We could lowercase the input in the other function
+    // Instead over here we'll just do it.
+    // (either method works)
+    //humanChoice = humanChoice.toLowercase();
+
+    // how many ways can we win?
+
+
+    // I suppose you want to split
+    // Player Wins
+
+    if 
+    ((humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper"))
     {
-        return "rock";
+        console.log("You win! " + humanChoice + " beats " + computerChoice);
+        humanScore += 1;
     }
-    else if (humanChoice == "paper")
+    else if 
+    ((humanChoice === "rock" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "rock"))
     {
-        return "paper";
+        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+        computerScore += 1;
     }
     else
     {
-        return "scissors";
+        console.log("draw.")
     }
 
+    // Player Loses
+
+
+    // Draw
 }
 
-// Call the function 
-// (testing prompt)
-console.log(getHumanChoice());
+function playGame()
+// For every game
+{
+    for (let i = 0; i < 5; i++)
+    {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+}
+
+playGame()
